59c11a30c98878e1c557d41a869b1a673b317098
Hady Salhab <hadysalhab1@gmail.com>
2020-01-08 15:05:19 +1100

    Adding ViewPager Adapter and Necessary StateList Drawable For TabLayout Icon

---
+++ app/src/main/java/com/android/myapplication/todo/adapters/DailyNotePagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.android.myapplication.todo.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.android.myapplication.todo.ui.NotesListFragment
+import com.android.myapplication.todo.ui.RemindersListFragment
+import java.lang.IndexOutOfBoundsException
+
+
+const val NOTES_LIST_PAGE_INDEX = 0
+const val REMINDERS_LIST_PAGE_INDEX = 1
+
+
+class DailyNotePagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    private val tabFragmentsCreator: Map<Int, () -> Fragment> = mapOf(
+        NOTES_LIST_PAGE_INDEX to { NotesListFragment() },
+        REMINDERS_LIST_PAGE_INDEX to { RemindersListFragment() }
+    )
+
+    override fun getItemCount(): Int = tabFragmentsCreator.size
+
+    override fun createFragment(position: Int): Fragment =
+        tabFragmentsCreator[position]?.invoke() ?: throw IndexOutOfBoundsException()
+
+}
--- app/src/main/java/com/android/myapplication/todo/ui/HomeViewPagerFragment.kt
+++ app/src/main/java/com/android/myapplication/todo/ui/HomeViewPagerFragment.kt
@@ -6,21 +6,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 
 import com.android.myapplication.todo.R
+import com.android.myapplication.todo.adapters.DailyNotePagerAdapter
+import com.android.myapplication.todo.adapters.NOTES_LIST_PAGE_INDEX
+import com.android.myapplication.todo.adapters.REMINDERS_LIST_PAGE_INDEX
+import com.android.myapplication.todo.databinding.FragmentHomeViewPagerBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import java.lang.IndexOutOfBoundsException
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeViewPagerFragment : Fragment() {
+    private lateinit var tabLayout:TabLayout
+    private lateinit var viewPager:ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_view_pager, container, false)
+        val binding = FragmentHomeViewPagerBinding.inflate(layoutInflater,container,false)
+        tabLayout = binding.tabs
+        viewPager = binding.viewPager
+        viewPager.adapter = DailyNotePagerAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.setIcon(getTabIcon(position))
+            tab.text = getTabTitle(position)
+        }.attach()
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        return binding.root
     }
+
+    private fun getTabIcon(position:Int):Int=
+         when(position){
+            NOTES_LIST_PAGE_INDEX -> R.drawable.ic_note_list_selector
+            REMINDERS_LIST_PAGE_INDEX->R.drawable.ic_reminder_list_selector
+            else->throw IndexOutOfBoundsException()
+        }
+
+    private fun getTabTitle(position: Int):String? =
+        when(position){
+            NOTES_LIST_PAGE_INDEX->getString(R.string.my_notes_list_title)
+            REMINDERS_LIST_PAGE_INDEX->getString(R.string.my_reminders_list_title)
+            else->null
+        }
 
 
 }
