f00c6cf00623b97d5f41aaddd3c5b1c40acd819f
Hady Salhab <hadysalhab1@gmail.com>
2020-01-09 16:59:56 +1100

    Adding Repository Instantiation to Koin

--- app/src/main/java/com/android/myapplication/todo/App.kt
+++ app/src/main/java/com/android/myapplication/todo/App.kt
@@ -2,7 +2,9 @@ package com.android.myapplication.todo
 
 import android.app.Application
 import com.android.myapplication.todo.data.db.NotesDataBase
+import com.android.myapplication.todo.repositories.NotesRepository
 import com.android.myapplication.todo.ui.HomeViewPagerViewModel
+import com.android.myapplication.todo.ui.list.NotesListViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -15,7 +17,14 @@ class App :Application(){
             //Database
             NotesDataBase.getInstance(androidContext())
         }
+        single{
+            val noteDb:NotesDataBase = get()
+            NotesRepository(noteDb.notesDao)
+        }
+
         viewModel { HomeViewPagerViewModel() }
+
+
 
     }
 
