f72ca3505e6ba709a9479a6470b253818783951a
Hady Salhab <hadysalhab1@gmail.com>
2020-01-09 20:41:43 +1100

    Adding NotesEditViewModel

---
+++ app/src/main/java/com/android/myapplication/todo/ui/edit/NotesEditViewModel.kt
@@ -0,0 +1,11 @@
+package com.android.myapplication.todo.ui.edit
+
+import androidx.lifecycle.ViewModel
+import com.android.myapplication.todo.repositories.NotesRepository
+
+class NotesEditViewModel (private val notesRepository: NotesRepository,
+                          private val noteId:Int) : ViewModel(){
+
+
+
+}
