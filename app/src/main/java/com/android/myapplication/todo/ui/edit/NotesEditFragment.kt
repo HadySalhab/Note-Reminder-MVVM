701852c150c1f88ec406484d06ba7e12c5242a9e
Hady Salhab <hadysalhab1@gmail.com>
2020-01-09 20:49:51 +1100

    Updating NoteEdit Fragment & ViewModel

--- app/src/main/java/com/android/myapplication/todo/ui/edit/NotesEditFragment.kt
+++ app/src/main/java/com/android/myapplication/todo/ui/edit/NotesEditFragment.kt
@@ -6,20 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 
 import com.android.myapplication.todo.R
+import com.android.myapplication.todo.databinding.FragmentNotesEditBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 /**
  * A simple [Fragment] subclass.
  */
 class NotesEditFragment : Fragment() {
+    private val args by navArgs<NotesEditFragmentArgs>()
+    private lateinit var binding:FragmentNotesEditBinding
+    private val viewModel:NotesEditViewModel by viewModel {
+        parametersOf(args.noteId)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_edit, container, false)
+        binding =FragmentNotesEditBinding.inflate(layoutInflater)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
 
