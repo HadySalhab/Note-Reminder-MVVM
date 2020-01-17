08ce5cc90a0fb6431e6b1d87b3eebdc9b9b74a12
Hady Salhab <hadysalhab1@gmail.com>
2020-01-17 11:56:21 +1100

    Updating DeleteDialogFragment, Title Message is passed at runtime

--- app/src/main/java/com/android/myapplication/todo/ui/dialogs/DeleteDialogFragment.kt
+++ app/src/main/java/com/android/myapplication/todo/ui/dialogs/DeleteDialogFragment.kt
@@ -6,23 +6,38 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.android.myapplication.todo.R
+import com.android.myapplication.todo.util.ARG_DELETE_MESSAGE
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class DeleteDialogFragment :DialogFragment(){
+class DeleteDialogFragment private constructor(): DialogFragment() {
     interface Callbacks {
         fun onPositiveButtonClick()
     }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-         super.onCreateDialog(savedInstanceState)
-        val clickListener = DialogInterface.OnClickListener{dialog,id->
+        super.onCreateDialog(savedInstanceState)
+        val deleteMessage = arguments?.getString(ARG_DELETE_MESSAGE)
+        val clickListener = DialogInterface.OnClickListener { dialog, id ->
             targetFragment?.let { fragment ->
                 (fragment as Callbacks).onPositiveButtonClick()
             }
         }
         val alertDialogBuilder = MaterialAlertDialogBuilder(requireContext())
-            .setTitle(R.string.delete_message)
-            .setPositiveButton(R.string.delete_dialog_positive_button,clickListener)
-            .setNegativeButton(R.string.delete_dialog_negative_button,null)
+            .setTitle(deleteMessage)
+            .setPositiveButton(R.string.delete_dialog_positive_button, clickListener)
+            .setNegativeButton(R.string.delete_dialog_negative_button, null)
         return alertDialogBuilder.create()
+    }
+
+    companion object {
+        fun newInstance(message: String): DeleteDialogFragment {
+            val args = Bundle().apply {
+                putString(ARG_DELETE_MESSAGE, message)
+            }
+
+            return DeleteDialogFragment().apply {
+                arguments = args
+            }
+        }
     }
 }
