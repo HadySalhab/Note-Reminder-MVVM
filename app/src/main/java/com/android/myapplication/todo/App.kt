e843ccc593ce9339c42c4e6f29b81bd1d6fcaba1
Hady Salhab <hadysalhab1@gmail.com>
2020-01-19 14:58:32 +1100

    Creating Notification Channel for the reminder

--- app/src/main/java/com/android/myapplication/todo/App.kt
+++ app/src/main/java/com/android/myapplication/todo/App.kt
@@ -1,6 +1,10 @@
 package com.android.myapplication.todo
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import com.android.myapplication.todo.data.db.NoteAndRemindersDB
 import com.android.myapplication.todo.repositories.Repository
 import com.android.myapplication.todo.ui.HomeViewPagerViewModel
@@ -56,5 +60,30 @@ class App : Application() {
             androidContext(this@App)
             modules(koinModule)
         }
+        createNotificationChannel(getString(R.string.reminder_notification_channel_id),
+            getString(R.string.reminder_notification_channel_name),
+            getString(R.string.reminder_notification_channel_description))
     }
+
+    fun createNotificationChannel(channelId: String, channelName: String,description:String){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.apply {
+                enableLights(true)
+                lightColor = Color.BLUE
+                enableVibration(true)
+            }
+            notificationChannel.description = description
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            ) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+
 }
+
